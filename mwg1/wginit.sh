5aba0dae5e47c834c5a2f3458623db1328bb0aeb
smdyan <info@salihof.ru>
2026-05-23 11:50:51 +0300

    compose - set name br-docker24, updated iptables policy, 2nd setup for internet vpn and site2site

--- mwg1/wginit.sh
+++ mwg1/wginit.sh
@@ -9,16 +9,6 @@ fi
 # Поднимаем WireGuard
 wg-quick up /mwg/wg0.conf
 
-# Настраиваем policy routing после появления wg0
-ip route replace default dev wg0 table 200
-
-ip rule show | grep -q "from 10.13.14.3 lookup 200" \
-  || ip rule add from 10.13.14.3/32 table 200
-
-echo "WireGuard started"
-wg show
-ip rule
-ip route show table 200
 
 # Чтобы контейнер не завершился
 sleep infinity
--- mwg2/srv.sh
+++
@@ -1,1 +0,0 @@
-while true; do echo -e "HTTP/1.1 200 OK\n\n GAMEOVER" | nc -l -w 1 8085; done
